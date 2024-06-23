54ac4b5663c07d3e86258ac0e7b8893a60f1627c
Kanhu Charan <kanhu2551996@gmail.com>
2024-06-23 15:50:01 +0530

    login screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,8 +2,14 @@ import { Stack } from "expo-router";
 import { ClerkProvider,SignedIn,SignedOut } from "@clerk/clerk-expo"
 import { Text } from "react-native";
 import LoginScreen from "./../components/LoginScreen"
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
+  useFonts({
+    'outfit':require('./../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-bold':require('./../assets/fonts/Outfit-Bold.ttf'),
+    'outfit-medium':require('./../assets/fonts/Outfit-Medium.ttf'),
+  })
   return (
     <ClerkProvider  publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <SignedIn>
--- components/LoginScreen.jsx
+++ components/LoginScreen.jsx
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { Colors } from '@/constants/Colors'
 
 const LoginScreen = () => {
   return (
     <View>
-      <Text>LoginScreen</Text>
+      <View style={{display:"flex",alignItems:'center',marginTop:100}}>
+      <Image source={require('./../assets/images/login.png')} style={{
+        width:220,
+        height:450,
+        borderRadius:20,
+        borderWidth:6,
+        borderColor:'#000',
+      }}/>
+      </View>
+      <View style={styles.subContainer}>
+        <Text style={{fontSize:30,fontFamily:'outfit-bold',textAlign:"center"}}>
+          Your Ultimate <Text style={{color:Colors.PRIMARY}}>Community Business Directory</Text> App
+        </Text>
+        <Text style={{fontSize:15,fontFamily:'outfit',textAlign:'center',marginVertical:15,color:Colors.GRAY}}>Find your favorite business near you and post your own business to your commubity</Text>
+        <TouchableOpacity style={styles.btn}>
+          <Text style={{textAlign:'center',color:"#fff",fontFamily:'outfit'}}>Let's Get Started</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
 export default LoginScreen
+
+const styles = StyleSheet.create({
+  subContainer:{
+    backgroundColor:"#fff",
+    padding:20,
+    marginTop:-20
+  },
+  btn:{
+    backgroundColor:Colors.PRIMARY,
+    padding:16,
+    borderRadius:99,
+    marginTop:20
+  }
+})
