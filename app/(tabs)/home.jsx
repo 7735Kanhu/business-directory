730a744f1f4e5f961ade66a99924180fa4453813
Kanhu Charan <kanhu2551996@gmail.com>
2024-06-25 08:15:43 +0530

    business list

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,25 +1,29 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
 import React from 'react'
 import Header from '../../components/Home/Header'
 import Slider from '../../components/Home/Slider'
 import Category from '../../components/Home/Category'
+import PopularBusiness from '../../components/Home/PopularBusiness'
 
 const home = () => {
   return (
-    <View>
+<ScrollView contentContainerStyle={{paddingBottom:20}}>
+    <View style={{flex:1}}>
 {/* Header */}
 <Header />
 
 {/* Slider */}
 <Slider />
 
+
 {/* Category */}
 <Category />
 
 {/* Papular Business list */}
-<Text></Text>
+<PopularBusiness />
 
     </View>
+</ScrollView>
   )
 }
 
--- components/Home/Category.jsx
+++ components/Home/Category.jsx
@@ -4,9 +4,13 @@ import { Colors } from '../../constants/Colors'
 import { collection, getDocs, query } from 'firebase/firestore'
 import { db } from '../../consig/FirebaseConfig'
 import CategoryItem from './CategoryItem'
+import { useRouter } from 'expo-router'
 
 const Category = () => {
     const [categoryList,setCategoryList] = useState([])
+    const router = useRouter();
+
+
     useEffect(()=>{
         getCategoryList();
     },[])
@@ -41,7 +45,7 @@ const Category = () => {
       showsHorizontalScrollIndicator={false}
       style={{marginLeft:20}}
       renderItem={({item,index})=>(
-        <CategoryItem category={item} key={index} onCategoryPress={(category)=>console.log(category)}/>
+        <CategoryItem category={item} key={index} onCategoryPress={(category)=>router.push('/businesslist/'+item.name)}/>
       )}
       />
     </View>
